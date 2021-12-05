4050d6ea395935ebcb7ac964f1e55c45e053af9c
Davrbek <dtuyliboev@gmail.com>
2021-12-05 13:32:28 +0500

    project add Map.vue

---
+++ muzey/src/views/Map.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="columns">
+      <div class="column">
+        Salom Dunyo
+      </div>
+      <div class="column">
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48517.348023662234!2d70.89610519415739!3d40.53420394782557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38baeec3c33d7347%3A0xc2c6c765f877a9de!2zUGFsYWNlIG9mIEtodWTDoXnDoXIgS2jDoW4!5e0!3m2!1sen!2s!4v1638692933089!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      </div>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home'
+}
+</script>
