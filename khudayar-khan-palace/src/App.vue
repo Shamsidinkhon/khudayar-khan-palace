8e8a09fd362456c62e662d2c8d6a8763fbbd412e
Elbek96 <asqarov-elbek96@mail.ru>
2021-12-12 15:32:41 +0500

    #9Search Field -> vue js component

---
+++ khudayar-khan-palace/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<search-bar/>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SearchBar
+  }
+}
+</script>
+
+<style>
+
+</style>
